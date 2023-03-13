ac271c3a18ba581d6115611d747ee68b5ee831a1
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-03-13 16:36:00 +0100

    completed scatterplot graph

--- data-visualization/scatterplot-graph/script.js
+++ data-visualization/scatterplot-graph/script.js
@@ -1,97 +1,96 @@
+const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
-// let data
-// let values = [];
+const width = 800;
+const height = 600;
+const padding = 40;
 
-// let heightScale
-// let xScale
-// let xAxisScale
-// let yAxisScale
+let values = [];
+let xScale
+let yScale
 
-// let width = 800;
-// let height = 600;
-// let padding = 40;
 
-// let svg = d3.select('svg')
+const svg = d3.select('svg')
 
-// let drawCanvas = () => {
-//     svg.attr('width', width);
-//     svg.attr('height', height);
-// };
+const drawCanvas = () => {
+    svg.attr('width', width);
+    svg.attr('height', height);
+};
 
-// let generateScales = () => {
-//     heightScale = d3.scaleLinear()
-//                     .domain([0, d3.max(values, (item) => item[1])])
-//                     .range([0, height - (2 * padding)]);
+let generateScales = () => {
+    xScale = d3.scaleLinear()
+                .domain([d3.min(values, item => item.Year - 1), d3.max(values, item => item.Year + 2)])
+                .range([padding, width - padding]);
 
-//     xScale = d3.scaleLinear()
-//                .domain([0, values.length - 1])
-//                .range([padding, width - padding]);
+    yScale = d3.scaleTime()
+                .domain([d3.min(values, item => new Date((item.Seconds) * 1000 - 10000)), d3.max(values, item => new Date(item.Seconds * 1000 + 10000))])
+                .range([padding ,height - padding])
 
-//     let datesArray = values.map((item) => new Date(item[0]));
+};
 
-//     xAxisScale = d3.scaleTime()
-//                    .domain([d3.min(datesArray), d3.max(datesArray)])
-//                    .range([padding, width - padding]);
+let drawPoints = () => {
+    svg.selectAll('circle')
+        .data(values)
+        .enter()
+        .append('circle')
+        .attr('class', 'dot')
+        .attr('r', 5)
+        .attr('data-xvalue', (item) => item.Year)
+        .attr('data-yvalue', (item) => new Date(item.Seconds * 1000))
+        .attr('cx', item => xScale(item.Year))
+        .attr('cy', item => yScale(new Date(item.Seconds * 1000)))
+        .attr('fill', item => item.Doping != "" ? 'darkred' : 'darkgreen')
+        .on('mouseover', (event, item) => {
+            tooltip.style('opacity', 0.9)
+                    .attr('data-year', item.Year)
+                    .style('left', event.pageX + 'px')
+                    .style('top', event.pageY - 10 + 'px')
+                    .html(
+                        item.Name +
+                        ': ' +
+                        item.Nationality +
+                        '<br/>' +
+                        'Year: ' +
+                        item.Year +
+                        ', Time: ' +
+                        item.Time +
+                        (item.Doping ? '<br/><br/>' + item.Doping : '')
+                    )
+        })
+        .on('mouseout', function() {
+            tooltip.style('opacity', 0);
+        });
+};
 
-//     yAxisScale = d3.scaleLinear()
-//                    .domain([0, d3.max(values, (item) => item[1])])
-//                    .range([height - padding, padding])
-// };
+let generateAxes = () => {
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format('d'));
 
-// let drawBars = () => {
-//     let tooltip = d3.select('#barchart')
-//                     .append('div')
-//                     .attr('id', 'tooltip')
-//                     .style('visibility', 'hidden')
+    let yAxis = d3.axisLeft(yScale)
+                    .tickFormat(d3.timeFormat('%M:%S'))
 
-//     svg.selectAll('rect')
-//        .data(values)
-//        .enter()
-//        .append('rect')
-//        .attr('class', 'bar')
-//        .attr('width', (width - (2 * padding)) / values.length)
-//        .attr('data-date', (item) => item[0])
-//        .attr('data-gdp', (item) => item[1])
-//        .attr('height', (item) => heightScale(item[1]))
-//        .attr('x', (item, index) => xScale(index))
-//        .attr('y', (item) => (height - padding) - heightScale(item[1]))
-//        .on('mouseover', (event, item) => {
-//             tooltip.transition()
-//                    .style('visibility', 'visible')
-//                    .style("top", (event.pageY) + "px")
-//                    .style("left", (event.pageX) + "px")
+    svg.append('g')
+       .call(xAxis)
+       .attr('id', 'x-axis')
+       .attr('transform', 'translate(0, ' + (height - padding) + ')');
 
-//             tooltip.text(item[1] + ' Billions USD, ' + item[0])
+    svg.append('g')
+       .call(yAxis)
+       .attr('id', 'y-axis')
+       .attr('transform', 'translate(' + padding + ', 0)')
+};
 
-//             document.querySelector('#tooltip').setAttribute('data-date', item[0])
-//             })
-//         .on('mouseout', (event, item) => {
-//             tooltip.transition()
-//                     .style('visibility', 'hidden');
-//         })
-// };
+let tooltip = d3.select('#scatterplot-graph')
+                .append('div')
+                .attr('class', 'tooltip')
+                .attr('id', 'tooltip')
+                .style('opacity', 0);
 
-// let generateAxes = () => {
-//     let xAxis = d3.axisBottom(xAxisScale);
-//     let yAxis = d3.axisLeft(yAxisScale);
-
-//     svg.append('g')
-//        .call(xAxis)
-//        .attr('id', 'x-axis')
-//        .attr('transform', 'translate(0, ' + (height - padding) + ')');
-
-//     svg.append('g')
-//        .call(yAxis)
-//        .attr('id', 'y-axis')
-//        .attr('transform', 'translate(' + padding + ', 0)')
-// };
-
-// d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-//   .then(data => {
-//     values = data.data;
-//     // console.log(values);
-//     drawCanvas();
-//     generateScales();
-//     drawBars();
-//     generateAxes();
-// });
+d3.json(url)
+  .then(data => {
+    values = data
+     
+    drawCanvas();
+    generateScales();
+    drawPoints();
+    generateAxes();
+});
